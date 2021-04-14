e2327b76bc1b7a779aae8d487f834f8cb4cd9d19
Nandana <79196523+Nandana1234689@users.noreply.github.com>
2021-04-14 17:52:42 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,33 @@
+class Chain {
+constructor(x,y){
+  var nandana ={
+ bodyA:x , 
+ pointB:y ,
+ stiffness : 0.05,
+ length : 5
+ 
+  }
+
+this.chain=Constraint.create(nandana)
+World.add(world,this.chain);
+
+}
+display(){
+
+strokeWeight(0)
+
+if( this.chain.bodyA ){
+line (this.chain.bodyA.position.x,this.chain.bodyA.position.y-20,this.chain.pointB.x,this.chain.pointB.y)
+
+}
+
+}
+
+removeBlock(){
+ this.chain.bodyA=null
+}
+
+
+
+
+}
